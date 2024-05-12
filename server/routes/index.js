052aeb2beaacfe3f098077c883530abec677a79b
casioneer <xomutman@gmail.com>
2024-05-12 22:22:02 +0300

    add hierarchy for product and cart

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,8 @@
 const router = require("express").Router();
 
-const hello = require("./hello");
-const huynya = require("./huynya");
 const product = require("./product");
 const cart = require("./cart")
 
-router.use("/hello", hello)
-router.use("/huynya", huynya)
 router.use("/product", product)
 router.use("/cart", cart)
 
