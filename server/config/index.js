7446758e7e236dd87399ad3869fec63af41a7b90
casioneer <xomutman@gmail.com>
2024-05-13 20:02:34 +0300

    before config dc run

--- server/config/index.js
+++ server/config/index.js
@@ -17,13 +17,11 @@ const database =
 const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${database}`;
 const pool = new Pool({
   connectionString,
-  /*
-    SSL is not supported in development
-    */
+
   ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
   end: () => pool.end(),
-};
+}; 
