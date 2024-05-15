dcbaf77913f243228661039f9ff296cb0a18f498
casioneer <xomutman@gmail.com>
2024-05-15 16:03:36 +0300

    after config and dc run

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+/* require("dotenv").config();
 const { Pool } = require("pg");
 
 // Выводим значения переменных окружения
@@ -24,4 +24,32 @@ const pool = new Pool({
 module.exports = {
   query: (text, params) => pool.query(text, params),
   end: () => pool.end(),
-}; 
+}; */
+require("dotenv").config();
+const { Pool } = require("pg");
+
+// const isProduction = process.env.NODE_ENV === "production";
+// const database =
+//     process.env.NODE_ENV === "test"
+//         ? process.env.POSTGRES_DB_TEST
+//         : process.env.POSTGRES_DB;
+
+// const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${database}`;
+const databaseUrl = process.env.DATABASE_URL
+console.log(databaseUrl);
+
+// const connectionString = `postgresql://user:databasepassword@localhost:7890/database`;
+// console.log(connectionString);
+
+const pool = new Pool({
+  connectionString: databaseUrl,
+  /*
+    SSL is not supported in development
+    */
+  // ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  end: () => pool.end(),
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 
 const product = require("./product");
 const cart = require("./cart");
+const users = require("./users")
+const order = require("./order")
 
 router.use("/product", product)
 router.use("/cart", cart)
+router.use("/users", users)
+router.use("/orders", order)
 
 module.exports = router;
