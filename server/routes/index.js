34051ff7d325ba1b0c4476b8ff9be2a0a10710a7
casioneer <xomutman@gmail.com>
2024-05-17 11:13:02 +0300

    before frontend update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 
-const product = require("./product");
-const cart = require("./cart");
+const product = require("./product")
+const cart = require("./cart")
 const users = require("./users")
 const order = require("./order")
+const auth = require("./auth")
 
 router.use("/product", product)
 router.use("/cart", cart)
 router.use("/users", users)
 router.use("/orders", order)
+router.use("/auth", auth)
 
 module.exports = router;
